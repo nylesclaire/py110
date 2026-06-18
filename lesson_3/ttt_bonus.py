dc3b3222b379135c1fab28c6bbea983f529e2745
nylesclaire <149444393+nylesclaire@users.noreply.github.com>
2026-06-18 14:00:46 +0000

    Added the rest of the bonus features to the Tic Tac Toe game, including keeping score, functionality for changing who makes the first move, and more readable formatting.

--- lesson_3/ttt_bonus.py
+++ lesson_3/ttt_bonus.py
@@ -2,14 +2,14 @@
 import random
 import os
 
-# no magical constants
+# no magical quantities
 INITIAL_EMPTY_SQUARE = " "
 HUMAN_MARK = "X"
 COMPUTER_MARK = "O"
+MIDDLE_SQUARE = 5
 FIRST_MOVE = 'choose'  # options for this constant are "computer",
 #                           "player", or "choose"
-GAMES_NEEDED_TO_WIN_MATCH = 5
-
+GAMES_NEEDED_TO_WIN_MATCH = 3
 
 WINNING_LINES = [
         [1, 2, 3], [4, 5, 6], [7, 8, 9], 
@@ -35,9 +35,15 @@ def join_or(lst, delim=', ', end_word='or'):
     
 
 # Display the board
-def display_board(board):
+def display_board(board, player_score, computer_score,
+                  current_game_number):
     os.system('clear')
 
+    prompt(f'''Game Number {current_game_number}
+    Remember, {GAMES_NEEDED_TO_WIN_MATCH} wins are needed to win the match.''')
+    print(f"")
+    prompt(f"You: {player_score} | Computer: {computer_score}")
+    print(f"")
     prompt(f"You are {HUMAN_MARK}. Computer is {COMPUTER_MARK}.")
     print(f"")
     print(f"----------|---------|----------")
@@ -77,8 +83,8 @@ def computer_choice(board):
     if not square:
         square = detect_at_risk_square(board, HUMAN_MARK)
     if not square:                      
-        if 5 in empty_squares(board):    
-            square = 5                  
+        if MIDDLE_SQUARE in empty_squares(board):    
+            square = MIDDLE_SQUARE               
     if not square:
         square = random.choice(empty_squares(board)) 
     board[square] = COMPUTER_MARK
@@ -110,55 +116,150 @@ def detect_winner(board):
             return 'Computer'
     return None
 
-def is_computer_first_move():
-    while True:
-        prompt("Would you like the computer to move first? y/n")
+def choosing_first_move():
+    while True: 
+        prompt('''In each game of the match, the player who gets to
+    move first will alternate. Select who will move first in
+    the first game. Enter c (computer) or m (me).''')
         first_move_choice = input().strip().lower()
-        if first_move_choice == "y":
-            return True
-        elif first_move_choice == "n":
-            return None
-        prompt("Sorry, that's not a valid choice.")
+        if first_move_choice == "c":
+            return 'computer'
+        elif first_move_choice == "m":
+            return 'player'
+        prompt("Sorry, that's not a valid choice. Enter c or m.")
+
+def play_again():
+    prompt("Play again? (y or n)")
+    answer = input().lower()
+    while answer not in ["y", "yes", "yup", "n", "no", "nope"]:
+        prompt("Please enter y or n")
+        answer = input().lower().strip()
+    return answer in ["y", "yes", "yup"]
+
+def choose_square(board, current_player):
+    if current_player == 'player':
+        player_choice(board)
+    else: 
+        computer_choice(board)
+
+def alternate_player(current_player):
+    if current_player == 'player':
+        return 'computer'
+    return 'player'
 
 
 
 
+# Main program loops
+def play_tic_tac_toe(player_score, computer_score,
+                     current_game_number, first_to_move):
 
-# Main program loop
-def play_tic_tac_toe():
+    board = new_empty_board()
+
+    current_player = first_to_move
+
+    display_board(board, player_score, computer_score, 
+                          current_game_number)
+
     while True:
-        board = new_empty_board()
+            
+        choose_square(board, current_player)
 
-        player_chose_comp_move_first = None
-        while FIRST_MOVE == 'choose':
-            player_chose_comp_move_first = is_computer_first_move()
+        current_player = alternate_player(current_player)
+
+        display_board(board, player_score, computer_score, 
+                current_game_number)
+        
+        if someone_won(board) or board_full(board):
             break
+            
+    if someone_won(board):
+        prompt(f"{detect_winner(board)} won!")
+    else:
+        prompt(f"It's a tie!")
 
-        while FIRST_MOVE == 'computer' or player_chose_comp_move_first:
-            computer_choice(board)
+    return detect_winner(board)
+
+
+def play_match():
+    current_game_number = 1
+    player_score = 0
+    computer_score = 0
+
+    first_to_move = FIRST_MOVE
+    if first_to_move == "choose":
+        first_to_move = choosing_first_move()
+
+    while True:
+        
+        winner = play_tic_tac_toe(player_score, computer_score, 
+                                    current_game_number, 
+                                    first_to_move)
+
+        current_game_number += 1
+
+        first_to_move = alternate_player(first_to_move)
+
+        match winner:
+            case 'Player':
+                player_score += 1
+            case 'Computer':
+                computer_score += 1
+            
+        if player_score == GAMES_NEEDED_TO_WIN_MATCH:
+            prompt(f"-------------------------------------")
+            prompt(r'''You are the 
+                           _            _                       _ 
+                          | |          (_)                     | |
+  __ _ _ __ __ _ _ __   __| | __      ___ _ __  _ __   ___ _ __| |
+ / _` | '__/ _` | '_ \ / _` | \ \ /\ / / | '_ \| '_ \ / _ \ '__| |
+| (_| | | | (_| | | | | (_| |  \ V  V /| | | | | | | |  __/ |  |_|
+ \__, |_|  \__,_|_| |_|\__,_|   \_/\_/ |_|_| |_|_| |_|\___|_|  (_)
+  __/ |                                                           
+ |___/                                                             
+                   ''')
             break
+        elif computer_score == GAMES_NEEDED_TO_WIN_MATCH:
+            prompt(f"-------------------------------------")
+            prompt(r'''
+ _   _                                            _                                       __
+| | | |                                          | |                                   _ / /
+| |_| |__   ___    ___ ___  _ __ ___  _ __  _   _| |_ ___ _ __  __      _____  _ __   (_) | 
+| __| '_ \ / _ \  / __/ _ \| '_ ` _ \| '_ \| | | | __/ _ \ '__| \ \ /\ / / _ \| '_ \    | | 
+| |_| | | |  __/ | (_| (_) | | | | | | |_) | |_| | ||  __/ |     \ V  V / (_) | | | |  _| | 
+ \__|_| |_|\___|  \___\___/|_| |_| |_| .__/ \__,_|\__\___|_|      \_/\_/ \___/|_| |_| (_) | 
+                                     | |                                                 \_\
+                                     |_|                                                    
+                   ''')
+            break   
 
-        while True:
-            display_board(board)
+        if not play_again():
+            return "DONE"
 
-            player_choice(board)
-            if someone_won(board) or board_full(board):
+
+def from_welcome_to_finish():
+    print(r'''
+ _____ _        _____            _____          
+|_   _(_)      |_   _|          |_   _|         
+  | |  _  ___    | | __ _  ___    | | ___   ___ 
+  | | | |/ __|   | |/ _` |/ __|   | |/ _ \ / _ \
+  | | | | (__    | | (_| | (__    | | (_) |  __/
+  \_/ |_|\___|   \_/\__,_|\___|   \_/\___/ \___|
+                                                
+                                                ''')
+
+    prompt(f'''Let's play Tic Tac Toe!\n
+    First to win {GAMES_NEEDED_TO_WIN_MATCH} games wins the match.
+    -------------------------------------'''
+    )
+    while True:
+        if play_match() == "DONE":
+            break
+        else: 
+            prompt("What an incredible match!")
+            if not play_again():
                 break
 
-            computer_choice(board)
-            if someone_won(board) or board_full(board):
-                break
-
-        display_board(board)
-
-        if someone_won(board):
-            prompt(f"{detect_winner(board)} won!")
-        else:
-            prompt(f"It's a tie!")
-        prompt("Play again? (y or n)")
-        answer = input().lower()
-        if answer[0] != 'y':
-            break
     prompt("Thanks for playing Tic Tac Toe!")
 
-play_tic_tac_toe()
+from_welcome_to_finish()
